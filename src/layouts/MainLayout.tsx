4435e71f98e88b8fa84d523e464fa04efec26f9e
Briand Alpa <briand.devs@gmail.com>
2026-03-10 13:40:41 +0700

    feat: build footer

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 
 interface MainLayoutProps {
@@ -9,6 +10,7 @@ export default function MainLayout({ children }: MainLayoutProps) {
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-1">{children}</main>
+      <Footer />
     </div>
   );
 }
